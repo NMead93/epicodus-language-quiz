b5dee8a1e9881a2c28d01d879095ef4f5f319beb
Nicholas Mead <nixter11@uw.edu>
2017-01-13 09:06:38 -0800

    implement jquery logic to traverse form questions. Hide css in effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
+    var nextCount = 0;
+    var pair;
+    var location;
+    var dev;
+    var size;
+    var date;
+
+    $('.btn-primary').click(function() {
+        $('.initial, .pair, .location, .dev, .size, .date').hide();
+        if (nextCount === 0) {
+            $('.pair').show();
+        } else if (nextCount === 1) {
+            pair = $("input:radio[name=pair]:checked").val();
+            if (pair === "Yes") {
+                $('.location').show();
+            } else {
+                $('.bad').show();
+            }
+        } else if(nextCount === 2) {
+            location = $("input:radio[name=location]:checked").val();
+            $('.dev').show();
+        } else if (nextCount === 3) {
+            dev = $("input:radio[name=dev]:checked").val();
+            $('.size').show();
+        } else {
+            size = $("input:radio[name=size]:checked").val();
+            $('.date').show();
+            $('.btn-primary').hide();
+            $('.btn-success').show();
+        }
+
+        nextCount++;
+    });
 
 });
